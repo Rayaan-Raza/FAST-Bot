e39732471b6292210bc345a9fce9469406cfc4dc
Rayaan <rayaanraza121@gmail.com>
2025-06-21 01:34:25 +0500

    model made

--- Model_training/model_training.py
+++ Model_training/model_training.py
@@ -12,7 +12,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  #Suppress TensorFlow warnings
 
 lemmatizer = WordNetLemmatizer()
 
-intents = json.loads(open('chatbot_intents.json'))
+with open('c:/Users/rayaan/Desktop/FAST-bot/Model_training/chatbot_intents.json', 'r') as file:
+    intents = json.load(file)
 
 words = []
 classes = []
@@ -29,5 +30,48 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
             
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters] 
- 
+words = [lemmatizer.lemmatize(w) for w in words if w not in ignore_letters] 
+
+words = sorted(set(words)) 
+
+classes = sorted(set(classes))
+
+os.makedirs('model', exist_ok=True)
+pickle.dump(words,open('model/words.pkl','wb'))
+pickle.dump(classes,open('model/classes.pkl','wb'))
+
+training = []
+output_empty = [0]*len(classes)
+
+
+for document in documents:
+    bag = []
+    word_pattern = document[0]
+    word_pattern = [lemmatizer.lemmatize(word) for word in word_pattern if word not in ignore_letters]
+
+    for word in words:
+        bag.append(1) if word in word_pattern else bag.append(0)
+        
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag,output_row])
+    
+random.shuffle(training)
+training = np.array(training, dtype=object)
+
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+model = Sequential()
+model.add(Dense(128,input_shape=(len(train_x[0]),),activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]),activation='softmax'))
+
+sgd = SGD(learning_rate=0.01, weight_decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('model/chatbot_model.keras')
+
