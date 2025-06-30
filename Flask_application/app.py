f6a6eaef3778bda9940cf8857c91803eb9c4fba6
Rayaan <rayaanraza121@gmail.com>
2025-06-30 14:30:21 +0500

    fixing deployment bug

--- Flask_application/app.py
+++ Flask_application/app.py
@@ -25,5 +25,5 @@ def handle_message():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  
-    app.run(host='0.0.0.0',debug=True)
+ port = int(os.environ.get("PORT", 5000))  # use Render's assigned port
+ app.run(host='0.0.0.0', port=port, debug=True)
